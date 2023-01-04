a363af41078f96c567c42127a74ca55602f943c2
Flavio <flaviohenriquefc@gmail.com>
2023-01-04 12:48:33 -0300

    Melhorando código e corrijindo bugs

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,7 +1,6 @@
-import { Console, log } from "console";
-import React, { ChangeEvent, DetailedHTMLProps, MouseEventHandler, useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import '../styles/Box.css'
-import { BorderRadius } from "../types/borderRadius";
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 
 export function Box() {
@@ -9,80 +8,66 @@ export function Box() {
     const [inputTopRight, setInputTopRight] = useState('0');
     const [inputBottomLeft, setInputBottomLeft] = useState('0');
     const [inputBottomRight, setInputBottomRight] = useState('0');
+    const [copied, setCopied] = useState(false);
 
-    function borderRadius(): BorderRadius{
-        const borders = {
+    function borderRadius() {
+        return {
             borderBottomLeftRadius: `${inputBottomLeft}px`,
             borderBottomRightRadius:  `${inputBottomRight}px`,
             borderTopLeftRadius: `${inputTopLeft}px`,
             borderTopRightRadius: `${inputTopRight}px`
         }
-        return borders;
     }
 
-    const handleInputTopLeft = (event:ChangeEvent) :void => {
+    const handleInputTopLeft = (event:ChangeEvent) => {
         let {value} = event.target as HTMLInputElement
         setInputTopLeft(value)
-        
     }
 
-    const handleInputTopRight = (event:ChangeEvent) :void => {
+    const handleInputTopRight = (event:ChangeEvent) => {
         let {value} = event.target as HTMLInputElement
         setInputTopRight(value)
     }
 
-    const handleInputBottomLeft = (event:ChangeEvent) :void => {
+    const handleInputBottomLeft = (event:ChangeEvent) => {
         let {value} = event.target as HTMLInputElement
         setInputBottomLeft(value)
     }
 
-    const handleInputBottomRight = (event:ChangeEvent) :void => {
+    const handleInputBottomRight = (event:ChangeEvent) => {
         let {value} = event.target as HTMLInputElement
         setInputBottomRight(value)
     }
 
-    const handleCopyText = () :void => {
-
-    }
-    const bl = () => {
-        return{
-            __html: `border-bottom-left-radius:${borderRadius().borderBottomLeftRadius};`
-
-        }
-    }
-    const br = () => {
-        return{
-            __html: `border-bottom-right-radius:${borderRadius().borderBottomRightRadius};`
-
-        }
-    }
-    const tl = () => {
-        return{
-            __html: `border-top-left-radius:${borderRadius().borderTopLeftRadius};`
-        }
-    }
-    const tr = () => {
-        return{
-            __html: `border-top-right-radius:${borderRadius().borderTopRightRadius};`
-        }
-    }
+    const [code, setCode] = useState();
+    
     return(
+        <>
         <div className="border_radius">
             <input type="number" name="border-top-left" placeholder="0" value={inputTopLeft} id="top_left" className="input_border" onChange={handleInputTopLeft}/>
             <input type="number" name="border-top-right" placeholder="0" value={inputTopRight} id="top_right" className="input_border" onChange={handleInputTopRight} />
             <div style={borderRadius()} className="caixa">
                 <div className='command'>
-                    <code dangerouslySetInnerHTML={bl()}></code>
-                    <br />
-                    <code dangerouslySetInnerHTML={br()}></code>
-                    <br />
-                    <code dangerouslySetInnerHTML={tl()}></code>
-                    <br />
-                    <code dangerouslySetInnerHTML={tr()}></code>
+                    <code>
+                        border-bottom-left-radius: {inputBottomLeft}px;
+                        border-bottom-right-radius:  {inputBottomRight}px;
+                        border-top-left-radius: {inputTopLeft}px;
+                        border-top-right-radius: {inputTopRight}px;
+                    </code>
                 </div>
             </div>
             <input type="number" name="border-bottom-left" placeholder="0" value={inputBottomLeft} id="bottom_left" className="input_border" onChange={handleInputBottomLeft} />
             <input type="number" name="border-bottom-right" placeholder="0" value={inputBottomRight} id="bottom_right" className="input_border" onChange={handleInputBottomRight} />
         </div>
+        <CopyToClipboard 
+        onCopy={()=> setCopied(true)}
+        options={{message:'copied!'}} 
+        text={  `border-bottom-left-radius: ${inputBottomLeft}px;
+                border-bottom-right-radius:  ${inputBottomRight}px;
+                border-top-left-radius: ${inputTopLeft}px;
+                border-top-right-radius: ${inputTopRight}px;` }>
+            <button>Copy code</button>
+        </CopyToClipboard>
+        </>
     );
 }
--- src/types/borderRadius.d.ts
+++
@@ -1,6 +0,0 @@
-export type BorderRadius = {
-    borderTopLeftRadius: string | number;
-    borderTopRightRadius: string | number;
-    borderBottomLeftRadius: string | number;
-    borderBottomRightRadius: string | number;
-}
